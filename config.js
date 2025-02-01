97eddee1bd921e71688cc8566c3c9c8ff8340b78
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-01 13:17:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID  || "VAJIRA-MD=IC9VlBaA#JZPLJb8c25l22lG2SSYoPSMyHHhT0_5sOuPO-JPWAlA",
 ALIVE_IMG: process.env.ALIVE_IMG || "url",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now MD back online 😈🚀🖤 ",
 };
