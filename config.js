f6b0959e912ea3e2e72795b1b6660877c0b8e1a1
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-02 12:14:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,41 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID  || "VAJIRA-MD=IC9VlBaA#JZPLJb8c25l22lG2SSYoPSMyHHhT0_5sOuPO-JPWAlA",
+SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tTfSqRbc/82.jpg",
-ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now MD back online 😈🚀🖤 ".
-"  *╭───────────╶╶╶╾⦁⦂⦁*".
-"*◈MENU MD_Chetiya_00◈*",
-"*╰───────────╶╶╶╾⦁⦂⦁*",
-"*👋 Hello Welcome to*
-"~`穴 ༺ KD᭄ chethiya 穴༺`~",
-͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏
-"*╭─────────────●●►*",
-"*┋ 1 | ᴅᴏᴡɴʟᴏᴀᴅ ᴄᴏᴍᴍᴀɴᴅs📥*",
-"*┋ 2 | ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅs🤴*",
-"*┋ 3 | ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅs👨‍💻*",
-"*┋ 4 | ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅs👨‍👦‍👦*",
-"*┋ 5 | ɴᴇᴡꜱ ᴄᴏᴍᴍᴀɴᴅs📰*",
-"*┋ 6 | ᴄᴏɴᴠᴇʀᴛ ᴄᴏᴍᴍᴀɴ🔮*",
-"*┋ 7 | ᴀɴɪᴍᴇ ᴄᴏᴍᴍᴀɴ💃*",
-"*┋ 8 | xᴠᴅʟ ᴄᴏᴍᴍᴀɴᴅ📵*",
-"*╰─────────────●●►*",
-
-"*[   PRIFIX : ( . )   ]*",
-"*╭─────────────●●►*",
-"*┋ 📜 .ALLMENU*",
-"*┋ ⚙️ .ALIVE*",
-"*╰─────────────●●►*",
-"*👨‍💻 ONLYE OWNERCOOMNDS*",
-💠 [ 94702484047  ] 🤴",
-
-"❯❯ 𝐍𝐔𝐌𝐁𝐄𝐑 𝐁𝐀𝐍𝐃 🔒",
-"❯❯ 𝐍𝐔𝐌𝐁𝐄𝐑 𝐔𝐍𝐁𝐀𝐍𝐃 🔒",
-"❯❯ 𝐇𝐄𝐑𝐎𝐊𝐔 𝐅𝐀𝐂𝐊𝐄 𝐂𝐀𝐑𝐃 🔒",
-"❯❯ 𝐒𝐏𝐀𝐌 🔒",
-"❯❯ 𝐁𝐔𝐆 🔒",
-
-"*•────────────╴╴╴•⟢*",
-" *© POWER BY MD chethiya wedasiga*",
-"*•────────────╴╴╴•⟢* ",
+ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive now MD bot back ",
 };
