4e79967af412404f395e0df5c13d06c7085abcb9
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-09 10:05:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,24 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID ||"".
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tTfSqRbc/82.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive now MD bot back ",
+ "╭───────────╶╶╶╾⦁⦂⦁",
+"ALIVE_KD_PANTA_00◈",
+"╰───────────🔲╶🔳╶╾⦁⦂",
+"•────────────╴╴╴•⟢",
+    "👋 HELLOW",
+"╰┈───────────╴╴╴•⟢*"
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏"JOINE MY WHATSAPP SONG GRUP🪢",
+    
+"♎│URL:* *https:// https://chat.whatsapp.com/Ci0kHZsu12mHbCk3jtQela",
+
+"╰┈───────────╴╴╴•⟢*",
+    
+"•────────────╴╴╴•⟢",
+
+"> *© POWER BY MD CHETHIUA* ",
+"╰┈───────────╴╴╴•⟢*",
+    
 };
