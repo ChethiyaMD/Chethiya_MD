ce305087b5d15c8bdfd6305f1646de37b8092339
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-14 19:58:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,34 +1,16 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+if (fs.existsSync('config.env')) require('dotenv').config({
+    path: './config.env'
+});
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/7JGtzTZR/117.jpg",
-ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive now MD bot back ",
- "╭───────────╶╶╶╾⦁⦂⦁",
-"ALIVE_KD_CHETHIYA_00 ◈",
-"╰───────────🔲╶🔳╶╾⦁⦂",
-"•────────────╴╴╴•⟢",
-    "👋 HELLOW",
-"╰┈───────────╴╴╴•⟢"
-͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏"JOINE MY WHATSAPP SONG GRUP🪢",
-    
-"♎│URL:*https://chat.whatsapp.com/Ci0kHZsu12mHbCk3jtQela",
-
-"╰┈───────────╴╴╴•⟢*",
-"*❖╭─────────────···▸*
-"> *TAIME🏷* : 10 hours, 53 minutes, 21 seconds",
-"> *RAM USE🧬* : 103.21MB / 63276MB🚀",
-"> NAME BOT©️: *MD Chethiya* ",
-"> *OWNER*🤴🏼 : *wa.me/94702484047*🛡",
-"> *CREATOR👨🏻‍🔧* : * MD ( chethiya wedasinga )* ",
-"> *VERSIONS🗃* :1.200.0v ",
-"•────────────╴╴╴•⟢",
-
-"> *© POWER BY MD CHETHIUA* ",
-"╰┈───────────╴╴╴•⟢*",
-    
+SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
+MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+ URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
+MAX_SIZE: 500
 };
