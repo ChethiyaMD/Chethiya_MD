3ba1174ca60fecf96612b2b05b71de88b194d57b
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-15 11:00:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+MONGODB: process.env.MONGODB || 'mongodb://mongo:McPSVfNkrNhTqaOOhExpUiRehdgUvYEM@trolley.proxy.rlwy.net:15431', //mongo 
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
