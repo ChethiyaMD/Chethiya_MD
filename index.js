83f471bd57dc982d5d6b99582ff5f1de9b7d2567
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-02-18 12:28:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Md  Bot connected to your whatsapp ✅🔳')
 
 let up = `Wa-chethiya BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/b7hkxj.jpg` }, caption: up })
 
 }
 })
